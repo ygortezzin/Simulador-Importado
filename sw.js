90635390118169b5e34883e1b503d7470ac981a1
ygortezzin <ygortezzin.fic@gmail.com>
2026-07-27 15:25:54 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'royalfic-fob-gasolina-v2';
+const CACHE = 'royalfic-fob-gasolina-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
